5a825e040dc753c2334a9138c4ffd1d4d225bd26
chaleay <elijahgaytan@utexas.edu>
2020-12-31 18:48:58 -0600

    finished java ds

--- 09-JS-DSA/codingchallenge.js
+++ 09-JS-DSA/codingchallenge.js
@@ -78,6 +78,7 @@ team2 > team1 && console.log(team2);
 */
 
 //1.
+/*
 for (const [index, name] of game.scored.entries()) {
   console.log(`Goal ${index + 1}: ${name}`);
 }
@@ -105,8 +106,8 @@ console.log(`Odd of victory of ${team1}: ${team1Odds}`);
 console.log(`Odd of draw: ${xOdds}`);
 console.log(`Odd of victory of ${team2}: ${team2Odds}`);
 OR  
-*/
 
+ 
 //have to use array notation for accesing object value here -> if we use dot notation, we'll be lookign for a property called 'team' which the game object doesn't have
 for (const [team, odd] of Object.entries(game.odds)) {
   const teamStr = team === 'x' ? 'draw' : `victory for ${game[team]}`;
@@ -115,3 +116,117 @@ for (const [team, odd] of Object.entries(game.odds)) {
 
 //4
 const scorers = {};
+for (const player of game.scored) {
+  scorers[player] = scorers[player]
+    ? (scorers[player] += 1)
+    : (scorers[player] = 1);
+}
+console.log(scorers);
+
+//HANDLING NESTED ARRAYS
+const entryPlayers = Object.entries(game.players);
+// console.log(entryPlayers);
+const [firstRoster, [, rosterNames]] = entryPlayers;
+console.log(rosterNames);
+*/
+
+//CC 3
+
+/* 
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events 
+themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+//1.
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+//2.
+gameEvents.delete(64);
+
+//3.
+const times = [...gameEvents.keys()];
+console.log(`Num Events :` + gameEvents.size);
+console.log(
+  `An event happened, on average, every ${
+    times[times.length - 1] / gameEvents.size
+  } minutes.`
+);
+
+//4.
+for (const [key, value] of gameEvents) {
+  console.log(
+    `${key <= 45 ? `[First Half]` : `[Second Half]`} ${key} : ${value}`
+  );
+}
+*/
+
+/*
+write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+*/
+
+//1.
+const textarea = document.createElement('textarea');
+const button = document.createElement('button');
+button.textContent = 'Submit';
+document.querySelector('.append').append(textarea);
+document.querySelector('.append').append(button);
+
+button.addEventListener('click', () => {
+  //split by new line
+  const textareaSplit = textarea.value.split('\n');
+  console.log(textareaSplit);
+  for (const text of textareaSplit) {
+    const textArray = text.trim().toLowerCase().split('_');
+    let secondPart = textArray[1];
+    secondPart = secondPart.replace(secondPart[0], secondPart[0].toUpperCase());
+    textArray[1] = secondPart;
+    let newText = textArray.join('');
+    console.log(newText);
+  }
+});
--- 09-JS-DSA/script.js
+++ 09-JS-DSA/script.js
@@ -84,6 +84,7 @@ const letters = [...str, ' ', 'S.'];
 console.log(...letters);
 */
 
+/*
 //Spread (since it occurs after equals sign)
 const arr = [1, 2, ...[3, 4]];
 //Rest (since it occurs before equals sign)
@@ -113,7 +114,7 @@ for (const item of menu) {
 for (const [i, el] of menu.entries()) {
   console.log(i, el);
 }
-*/
+/*
 
 //need to check if it exists, otherwise undefined error
 console.log(restaurant.openingHours.mon && restaurant.openingHours.mon.open);
@@ -135,3 +136,84 @@ console.log(entries);
 for (const [key, { open, close }] of entries) {
   console.log(`On ${key} we open at ${open} and close at ${close}.`);
 }
+
+*/
+
+/*
+//MAPS -
+const rest = new Map();
+rest.set('name', 'McDonalds');
+rest.set(1, 'My Backyard, Texas');
+rest.set(2, 'Lesbian, Portugal');
+console.log(rest);
+
+// or
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('home')); //undefined
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(rest.get('open') <= time && rest.get('close') > time));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+console.log(rest);
+console.log(rest.size);
+
+rest.set([1, 2], 'Test');
+console.log(rest);
+
+//Returns undefined. Even though the arrays contain the same elements, they do not point to the same elements in memory
+console.log(rest.get([1, 2]));
+
+//Solution 1:
+const arr = [1, 2];
+
+rest.set(arr, 'Test');
+console.log(rest.get(arr));
+
+//With dom
+rest.set(document.querySelector('h1'), 'Heading');
+*/
+
+//STRINGS
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+
+//Get size of string
+console.log(airline.length);
+
+//Get index
+console.log(airline.indexOf('z'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('Portugal')); //case sensitive
+
+//position at which extraction starts
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2)); //al
+console.log(airline.slice(1, -1)); //prints out AP air Portuga
+
+const checkMiddleSeat = function (seat) {
+  const char = seat.slice(seat.length - 1);
+  return char === 'B' || char === 'E';
+};
+
+console.log(checkMiddleSeat('11B') ? 'Middle Seat' : 'Good Seat!');
+console.log(checkMiddleSeat('11E') ? 'Middle Seat' : 'Good Seat!');
+console.log(checkMiddleSeat('11C') ? 'Middle Seat' : 'Good Seat!');
