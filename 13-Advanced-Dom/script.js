8accbbc8035133b595cb33313eae35f64ad29a1a
chaleay <elijahgaytan@utexas.edu>
2021-01-18 22:13:51 -0600

    starting tabbed componenet

--- 13-Advanced-Dom/script.js
+++ 13-Advanced-Dom/script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -32,9 +34,6 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
   console.log(s1coords);
@@ -54,14 +53,57 @@ btnScrollTo.addEventListener('click', function (e) {
   //however, this solution only really works when at very top of page,
   //if you scroll down distance from top of viewport to section is a lot less
   //window.scrollTo(s1coords.left, s1coords.top + window.pageYOffset);
-  // window.scrollTo({
-  // left: s1coords.left + window.pageXOffset, //should be 0
-  // top: s1coords.top + window.pageYOffset,
-  // behavior: 'smooth',
-  // });
+  window.scrollTo({
+    left: s1coords.left + window.pageXOffset, //should be 0
+    top: s1coords.top + window.pageYOffset,
+    behavior: 'smooth',
+  });
 
-  section1.scrollIntoView({ behavior: 'smooth' });
+  //section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+//page navigation//
+
+//new solution
+//We're doing it a more efficient way
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  //only do logic if classlist contains link
+  if (e.target.classList.contains('nav__link')) {
+    const idToScrollTo = e.target.getAttribute('href'); //returns section id -> i.e #section--1, section--2,
+    const coords = document.querySelector(idToScrollTo).getBoundingClientRect();
+    window.scrollTo({
+      left: coords.left + window.pageXOffset, //should be 0
+      top: coords.top + window.pageYOffset,
+      behavior: 'smooth',
+    });
+  }
+});
+
+///////////////////
+/*
+//OLD SCHOOL - DONT DO
+// h1.onmouseenter = function (e) {
+// alert('mouse enter h1 w/o function');
+// };
+
+//Types of events and event handlers
+const h1 = document.querySelector('h1');
+
+//listen once
+const alertH1 = e => {
+  alert('mouse enter h1');
+
+  //remove
+  // h1.removeEventListener('mouseenter', alertH1);
+};
+
+//DO THIS - better
+h1.addEventListener('mouseenter', alertH1);
+
+//can also do this
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 
 /*
 
@@ -153,3 +195,30 @@ logo.classList.contains();
 logo.className = '';
 /////////////////////////////
 */
+
+// const h1 = document.querySelector('h1');
+
+//going downards - selecting children
+// console.log(h1.querySelectorAll('.highlight'));
+// console.log(h1.children); //live collection
+// console.log(h1.childNodes);
+// h1.firstElementChild.style.color = 'white';
+// h1.lastElementChild.style.color = 'orangered';
+
+//Going upwards: parents
+// console.log(h1.parentNode); //the div
+// console.log(h1.parentElement); //the div
+//selects the closet parent header to our h1
+//h1.closest('.header').style.background = 'var(--color-primary)';
+
+//going sideways or selecting siblings
+// console.log(h1.previousElementSibling);
+// console.log(h1.nextElementSibling);
+
+//getting all siblings
+// console.log(h1.parentElement.children);
+// [...h1.parentElement.children].forEach(function (el) {
+// if (el !== h1) {
+// el.style.transform = 'scale(.1)';
+// }
+// });
